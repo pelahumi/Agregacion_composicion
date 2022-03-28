7cf061010aadc1e6680d5b9d1636f655392cf763
Pelayo <phuermij@myuax.com>
2022-03-28 20:45:07 +0200

    vuelta a empezar

---
+++ clases/dia_mañana.py
@@ -0,0 +1,20 @@
+class NuevaYork():
+
+    def __init__(self, edificios):
+        self.edificios = []
+        self.edificios = []
+
+class Empresa():
+
+    def __init__(self):
+        self.propietario = "YooHoo"
+
+class Edificios():
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+class Empleados():
+
+    def __init__(self, nombre):
+        self.nombre = nombre
