bd9f988e59451d9ccd686b2e11315123195c1480
Pelayo <phuermij@myuax.com>
2022-03-30 19:54:17 +0200

    clase nueva york y los angeles

--- clases/dia_mañana.py
+++ clases/dia_mañana.py
@@ -4,6 +4,18 @@ class NuevaYork():
         self.edificios = [Edificios(nombre) for nombre in ["Martin", "Salin"]]
         self.edificios = [Empleados(nombre) for nombre in ["A", "B"]]
 
+    def __del__(self):
+        print("Nueva York fue destruido")
+
+class LosAngeles():
+
+    def __init__(self):
+        self.edificios = [Edificios(nombre) for nombre in ["Xing"]]
+        self.edificios = [Empleados(nombre) for nombre in ["C"]]
+
+    def __del__(self):
+        print("Los Ángeles fue destruido")
+
 class Empresa():
 
     def __init__(self):
